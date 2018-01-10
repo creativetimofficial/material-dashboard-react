f9a7ec74b95d5f6b74f35c91196e970b126f19f9
Manu <emanuelnazare@yahoo.ro>
2018-01-10 19:26:36 +0200

    added dynamic colors to chartCard

--- src/components/Cards/ChartCard.jsx
+++ src/components/Cards/ChartCard.jsx
@@ -29,9 +29,26 @@ const styles = {
         fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
         fontWeight: '300',
         lineHeight: '1.5em',
+    },
+    cardHeaderOrange: {
+        background: 'linear-gradient(60deg, #ffa726, #fb8c00)',
+        boxShadow: '0 12px 20px -10px rgba(255, 152, 0, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(255, 152, 0, 0.2)',
+    },
+    cardHeaderGreen: {
         background: 'linear-gradient(60deg, #66bb6a, #43a047)',
         boxShadow: '0 12px 20px -10px rgba(76, 175, 80, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(76, 175, 80, 0.2)',
-
+    },
+    cardHeaderRed: {
+        background: 'linear-gradient(60deg, #ef5350, #e53935)',
+        boxShadow: '0 12px 20px -10px rgba(244, 67, 54, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(244, 67, 54, 0.2)',
+    },
+    cardHeaderBlue: {
+        background: 'linear-gradient(60deg, #26c6da, #00acc1)',
+        boxShadow: '0 12px 20px -10px rgba(0, 188, 212, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 188, 212, 0.2)',
+    },
+    cardHeaderPurple: {
+        background: 'linear-gradient(60deg, #ab47bc, #8e24aa)',
+        boxShadow: '0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)',
     },
     cardContent: {
         padding: '15px 20px',
@@ -81,34 +98,82 @@ const styles = {
         width: '16px',
         height: '16px',
     },
+    cardStatsIconWarning: {
+        color: '#ff9800',
+    },
+    cardStatsIconPrimary: {
+        color: '#9c27b0',
+    },
+    cardStatsIconDanger: {
+        color: '#f44336',
+    },
+    cardStatsIconSuccess: {
+        color: '#4caf50',
+    },
+    cardStatsIconInfo: {
+        color: '#00bcd4',
+    },
+    cardStatsIconRose: {
+        color: '#e91e63',
+    },
+    cardStatsIconGray: {
+        color: '#999999',
+    },
 };
 
 class StatsCard extends React.Component{
     constructor(props){
         super(props);
-        // switch (this.props.statIconColor) {
-        //     case 'warning':
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconWarning
-        //         break;
-        //     case 'primary':
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconPrimary
-        //         break;
-        //     case 'danger':
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconDanger
-        //         break;
-        //     case 'success':
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconSuccess
-        //         break;
-        //     case 'info':
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconInfo
-        //         break;
-        //     case 'rose':
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconRose
-        //         break;
-        //     default:
-        //         this.state.statIconColor = " " + this.props.classes.cardStatsIconGray
-        //     break;
-        // }
+        switch (this.props.iconColor) {
+            case 'orange':
+                this.state = {
+                    iconColor: " " + this.props.classes.cardHeaderOrange
+                }
+                break;
+            case 'green':
+                this.state = {
+                    iconColor: " " + this.props.classes.cardHeaderGreen
+                }
+                break;
+            case 'red':
+                this.state = {
+                    iconColor: " " + this.props.classes.cardHeaderRed
+                }
+                break;
+            case 'blue':
+                this.state = {
+                    iconColor: " " + this.props.classes.cardHeaderBlue
+                }
+                break;
+            default:
+                this.state = {
+                    iconColor: " " + this.props.classes.cardHeaderPurple
+                }
+            break;
+        };
+        switch (this.props.statIconColor) {
+            case 'warning':
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconWarning
+                break;
+            case 'primary':
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconPrimary
+                break;
+            case 'danger':
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconDanger
+                break;
+            case 'success':
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconSuccess
+                break;
+            case 'info':
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconInfo
+                break;
+            case 'rose':
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconRose
+                break;
+            default:
+                this.state.statIconColor = " " + this.props.classes.cardStatsIconGray
+            break;
+        }
     }
     render(){
         return (
@@ -116,7 +181,7 @@ class StatsCard extends React.Component{
                 <Card className={this.props.classes.card}>
                     <CardHeader
                         classes={{
-                            root: (this.props.classes.cardHeader),
+                            root: (this.props.classes.cardHeader + this.state.iconColor),
                             // avatar: this.props.classes.cardAvatar
                         }}
                         subheader={
