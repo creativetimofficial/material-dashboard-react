3fc9f6d5b3c3787b73944b264b8a960731a73ecb
Manu <emanuelnazare@yahoo.ro>
2018-01-16 09:14:21 +0200

    added index in components folder for a better way of importing components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,59 @@
+// ##############################
+// // // Cards
+// #############################
+import ChartCard from './Cards/ChartCard.jsx';
+import ProfileCard from './Cards/ProfileCard.jsx';
+import RegularCard from './Cards/RegularCard.jsx';
+import StatsCard from './Cards/StatsCard.jsx';
+import TasksCard from './Cards/TasksCard.jsx';
+
+// ##############################
+// // // CustomButtons
+// #############################
+
+import Button from './CustomButtons/Button.jsx';
+import IconButton from './CustomButtons/IconButton.jsx';
+
+// ##############################
+// // // CustomInput
+// #############################
+
+import CustomInput from './CustomInput/CustomInput.jsx';
+
+// ##############################
+// // // Footer
+// #############################
+
+import Footer from './Footer/Footer.jsx';
+
+// ##############################
+// // // Header
+// #############################
+
+import Header from './Header/Header.jsx';
+
+// ##############################
+// // // Sidebar
+// #############################
+
+import Sidebar from './Sidebar/Sidebar.jsx';
+
+// ##############################
+// // // Table
+// #############################
+
+import Table from './Table/Table.jsx';
+
+// ##############################
+// // // Tasks
+// #############################
+
+import Tasks from './Tasks/Tasks.jsx';
+
+// ##############################
+// // // Typography
+// #############################
+
 import H1 from "./Typography/H1.jsx";
 import H2 from "./Typography/H2.jsx";
 import H3 from "./Typography/H3.jsx";
@@ -15,19 +71,22 @@ import Danger from "./Typography/Danger.jsx";
 import Small from "./Typography/Small.jsx";
 
 export {
-    H1,
-    H2,
-    H3,
-    H4,
-    H5,
-    H6,
-    P,
-    Quote,
-    Muted,
-    Primary,
-    Info,
-    Success,
-    Warning,
-    Danger,
-    Small,
+    // Cards
+    ChartCard, ProfileCard, RegularCard, StatsCard, TasksCard,
+    // CustomButtons
+    Button, IconButton,
+    // CustomInput
+    CustomInput,
+    // Footer
+    Footer,
+    // Header
+    Header,
+    // Sidebar
+    Sidebar,
+    // Table
+    Table,
+    // Tasks
+    Tasks,
+    // Typography
+    H1, H2, H3, H4, H5, H6, P, Quote, Muted, Primary, Info, Success, Warning, Danger, Small,
 };
