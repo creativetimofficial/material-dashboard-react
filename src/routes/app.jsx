08c5c429df54576a684fc022e6544b7e14962a0c
Manu <emanuelnazare@yahoo.ro>
2018-01-19 11:48:47 +0200

    deleted testing page Notify

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -6,11 +6,6 @@ import Icons from "views/Icons/Icons.jsx";
 import Maps from "views/Maps/Maps.jsx";
 import NotificationsPage from "views/Notifications/Notifications.jsx";
 
-
-
-import Notify from 'views/Notify/Notify.jsx';
-
-
 import {
     Dashboard, Person, ContentPaste, LibraryBooks, BubbleChart, LocationOn, Notifications
 } from 'material-ui-icons';
@@ -23,7 +18,6 @@ const appRoutes = [
     { path: "/icons", sidebarName: "Icons", navbarName: "Icons", icon: BubbleChart, component: Icons },
     { path: "/maps", sidebarName: "Maps", navbarName: "Map", icon: LocationOn, component: Maps },
     { path: "/notifications", sidebarName: "Notifications", navbarName: "Notifications", icon: Notifications, component: NotificationsPage },
-    { path: "/notify", sidebarName: "Notify", navbarName: "Notify", icon: Notifications, component: Notify },
     { redirect: true, path: "/", to: "/dashboard" }
 ];
 
