de3a702ff0306a9d4bad19361596617a59a9bf44
Manu <emanuelnazare@yahoo.ro>
2018-01-22 13:35:20 +0200

    work on sidebar and header for documentation

--- src/containers/Documentation/DocSidebar/DocSidebar.jsx
+++ src/containers/Documentation/DocSidebar/DocSidebar.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+    withStyles, List, ListItem, ListItemText
+} from 'material-ui';
+
+const style = {
+    list: {
+        paddingTop: '15px',
+        paddingRight: '15px',
+        paddingLeft: '15px',
+        paddingBottom: '.25rem',
+        borderRight: '1px solid rgba(0,0,0,.1)',
+    },
+    listItemText: {
+        display: 'block',
+        fontSize: '90%',
+        color: 'rgba(0,0,0,.65)',
+        fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+        fontWeight: '300',
+        lineHeight: '1.5em',
+    },
+    active: {
+        fontWeight: '500',
+        color: 'rgba(0,0,0,.85)',
+        backgroundColor: 'transparent'
+    },
+    navLink: {
+        textDecoration: 'none',
+    },
+    listItem: {
+        paddingTop: '.25rem',
+    }
+};
 
 class DocSidebar extends React.Component{
+    activeRoute(routeName) {
+        return this.props.location.pathname.indexOf(routeName) > -1 ? true : false;
+    }
     render(){
+        const { classes, routes } = this.props;
         return (
-            <div>
-                salut din DocSidebar
-            </div>
+            <List className={classes.list}>
+                {
+                    routes.map((prop,key) => {
+                        if(prop.redirect)
+                            return null;
+                        return (
+                            <NavLink to={prop.path} activeClassName="active" key={key} className={classes.navLink}>
+                                <ListItem className={classes.listItem}>
+                                    <ListItemText primary={prop.name} disableTypography={true} className={classes.listItemText + " " + (this.activeRoute(prop.path) ? classes.active:"")}/>
+                                </ListItem>
+                            </NavLink>
+                        );
+                    })
+                }
+            </List>
         );
     }
 }
 
-export default DocSidebar;
+export default withStyles(style)(DocSidebar);
--- src/containers/Documentation/Documentation.jsx
+++ src/containers/Documentation/Documentation.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import {
+    Grid
+} from 'material-ui';
+import {
+    Switch,
+    Route,
+    Redirect
+} from 'react-router-dom';
 
+
+import DocHeader from './DocHeader/DocHeader';
+import DocSidebar from './DocSidebar/DocSidebar';
+
+import docRoutes from 'routes/documentation.jsx';
 
 class Documentation extends React.Component{
     render(){
         return (
             <div>
-                Hey
+                <DocHeader />
+                <Grid container style={{marginTop: "75px"}}>
+                    <Grid item xs={12} sm={12} md={3} lg={2} xl={2}>
+                        <DocSidebar routes={docRoutes} {...this.props}/>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={9} lg={10} xl={10}>
+                        <Switch>
+                            {
+                                docRoutes.map((prop,key) => {
+                                    if(prop.redirect)
+                                        return (
+                                            <Redirect from={prop.path} to={prop.to} key={key}/>
+                                        );
+                                    return (
+                                        <Route path={prop.path} component={prop.component} key={key}/>
+                                    );
+                                })
+                            }
+                        </Switch>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
