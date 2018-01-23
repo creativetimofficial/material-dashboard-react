cfea76b9a9a1373b21379d707583f88d236f10c2
Manu <emanuelnazare@yahoo.ro>
2018-01-23 12:08:33 +0200

    working on Buttons

--- src/containers/Documentation/Sections/Buttons.jsx
+++ src/containers/Documentation/Sections/Buttons.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+import { prism } from 'react-syntax-highlighter/styles/prism';
+import { Favorite } from 'material-ui-icons';
+
+import {
+    Button, IconButton, P
+} from 'components';
+
+const importExamples = `import { Button } from 'components';`;
+
+const examples = `<Button type="button" color="primary">Primary</Button>
+<Button type="button" color="info">Info</Button>
+<Button type="button" color="success">Success</Button>
+<Button type="button" color="danger">Danger</Button>
+<Button type="button" color="warning">Warning</Button>
+<Button type="button" color="rose">Rose</Button>`
+
+const importStyle = `import { Favorite } from 'material-ui-icons';
+import { Button, IconButton } from 'components';`;
+
+const style = `<Button color="primary">Default</Button>
+<Button color="primary" round>Round</Button>
+<Button color="primary" round><Favorite /> With Icon</Button>
+<IconButton color="primary"><Favorite style={{color: "#FFFFFF"}}/></IconButton>
+<Button color="transparent">Simple</Button>`;
 
 class Buttons extends React.Component{
     render(){
         return (
             <div>
-                salut din Buttons
+                <h1>Buttons</h1>
+                <P>Use Material Dashboard's React custom button styles for actions in forms, dialogs, and more with support for multiple states, and more.</P>
+                <h2>Examples</h2>
+                <P>Material Dashboard React has changed the predefined button styles from Material UI, each serving its own semantic purpose, with a few extras thrown in for more control.</P>
+                <Button type="button" color="primary">Primary</Button>
+                <Button type="button" color="info">Info</Button>
+                <Button type="button" color="success">Success</Button>
+                <Button type="button" color="danger">Danger</Button>
+                <Button type="button" color="warning">Warning</Button>
+                <Button type="button" color="rose">Rose</Button>
+                <SyntaxHighlighter language="jsx" style={prism}>{importExamples}</SyntaxHighlighter>
+                <SyntaxHighlighter language="jsx" style={prism}>{examples}</SyntaxHighlighter>
+                <h2>Style Buttons</h2>
+                <Button color="primary">Default</Button>
+                <Button color="primary" round>Round</Button>
+                <Button color="primary" round><Favorite /> With Icon</Button>
+                <IconButton color="primary"><Favorite style={{color: "#FFFFFF"}}/></IconButton>
+                <Button color="transparent">Simple</Button>
+                <SyntaxHighlighter language="jsx" style={prism}>{importStyle}</SyntaxHighlighter>
+                <SyntaxHighlighter language="jsx" style={prism}>{style}</SyntaxHighlighter>
             </div>
         );
     }
--- src/containers/Documentation/Sections/Tutorial.jsx
+++ src/containers/Documentation/Sections/Tutorial.jsx
@@ -6,6 +6,100 @@ import {
     P
 } from 'components';
 
+const files = `material-dashboard-react
+├── LICENSE.md
+├── README.md
+├── package-lock.json
+├── package.json
+├── public
+│   ├── apple-icon.png
+│   ├── favicon.ico
+│   ├── index.html
+│   └── manifest.json
+└── src
+    ├── assets
+    │   ├── css
+    │   │   └── material-dashboard-react.css
+    │   └── img
+    │       ├── apple-icon.png
+    │       ├── cover.jpeg
+    │       ├── faces
+    │       │   └── marc.jpg
+    │       ├── favicon.png
+    │       ├── mask.png
+    │       ├── new_logo.png
+    │       ├── reactlogo.png
+    │       ├── sidebar-1.jpg
+    │       ├── sidebar-2.jpg
+    │       ├── sidebar-3.jpg
+    │       ├── sidebar-4.jpg
+    │       └── tim_80x80.png
+    ├── components
+    │   ├── Cards
+    │   │   ├── ChartCard.jsx
+    │   │   ├── ProfileCard.jsx
+    │   │   ├── RegularCard.jsx
+    │   │   ├── StatsCard.jsx
+    │   │   └── TasksCard.jsx
+    │   ├── CustomButtons
+    │   │   ├── Button.jsx
+    │   │   └── IconButton.jsx
+    │   ├── CustomInput
+    │   │   └── CustomInput.jsx
+    │   ├── Footer
+    │   │   └── Footer.jsx
+    │   ├── Header
+    │   │   ├── Header.jsx
+    │   │   └── HeaderLinks.jsx
+    │   ├── Sidebar
+    │   │   └── Sidebar.jsx
+    │   ├── Snackbar
+    │   │   ├── Snackbar.jsx
+    │   │   └── SnackbarContent.jsx
+    │   ├── Table
+    │   │   └── Table.jsx
+    │   ├── Tasks
+    │   │   └── Tasks.jsx
+    │   ├── Typography
+    │   │   ├── A.jsx
+    │   │   ├── Danger.jsx
+    │   │   ├── Info.jsx
+    │   │   ├── Muted.jsx
+    │   │   ├── P.jsx
+    │   │   ├── Primary.jsx
+    │   │   ├── Quote.jsx
+    │   │   ├── Small.jsx
+    │   │   ├── Success.jsx
+    │   │   └── Warning.jsx
+    │   └── index.js
+    ├── containers
+    │   └── App
+    │       └── App.jsx
+    ├── index.js
+    ├── logo.svg
+    ├── routes
+    │   ├── app.jsx
+    │   └── index.jsx
+    ├── variables
+    │   ├── charts.jsx
+    │   ├── general.jsx
+    │   └── styles.jsx
+    └── views
+        ├── Dashboard
+        │   └── Dashboard.jsx
+        ├── Icons
+        │   └── Icons.jsx
+        ├── Maps
+        │   └── Maps.jsx
+        ├── Notifications
+        │   └── Notifications.jsx
+        ├── TableList
+        │   └── TableList.jsx
+        ├── Typography
+        │   └── Typography.jsx
+        └── UserProfile
+            └── UserProfile.jsx`;
+
 class Tutorial extends React.Component{
     render(){
         return (
@@ -57,6 +151,8 @@ class Tutorial extends React.Component{
                 </ul>
                 <P>After these steps navigate in your project to <code>views/Maps/Maps.jsx</code> and replace the <code>YOUR_KEY_HERE</code>, from line 29, with the given <code>API KEY</code>.</P>
                 <P>For more information regarding google maps API KEY navigate to this <a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank" rel="noopener noreferrer">link</a>.</P>
+                <h2>Files and Folders structure</h2>
+                <SyntaxHighlighter language="jsx" style={prism}>{files}</SyntaxHighlighter>
             </div>
         );
     }
