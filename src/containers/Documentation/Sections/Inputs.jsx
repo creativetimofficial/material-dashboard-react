f5d68e874a90fabaa29f982deb43e6447eb11206
Manu <emanuelnazare@yahoo.ro>
2018-01-25 11:10:53 +0200

    work on doc

--- src/containers/Documentation/Sections/Inputs.jsx
+++ src/containers/Documentation/Sections/Inputs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { prism } from 'react-syntax-highlighter/styles/prism';
+import { Grid } from 'material-ui';
 
 import { CustomInput } from 'components';
 
@@ -9,13 +10,67 @@ class Inputs extends React.Component{
         return (
             <div>
                 <h1>Inputs</h1>
-                <CustomInput
-                    labelText="Username"
-                    id="username"
-                    // inputProps={{
-                    //     error: true
-                    // }}
-                />
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <CustomInput
+                            labelText="Disabled"
+                            id="disabled"
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                            inputProps={{
+                                disabled: true
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <CustomInput
+                            id="regular"
+                            inputProps={{
+                                placeholder: "Regular"
+                            }}
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <CustomInput
+                            labelText="With floating label"
+                            id="float"
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <CustomInput
+                            labelText="Success input"
+                            id="success"
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <CustomInput
+                            labelText="Error input"
+                            id="error"
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <CustomInput
+                            labelText="Error input"
+                            id="error"
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                        />
+                    </Grid>
+                </Grid>
                 <SyntaxHighlighter language="jsx" style={prism}>{`<NameOfTheIcon />`}</SyntaxHighlighter>
             </div>
         );
