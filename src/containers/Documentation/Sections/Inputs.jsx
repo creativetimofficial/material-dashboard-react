e6817c46433798d150e4a231bc24244c3e351ad7
Manu <emanuelnazare@yahoo.ro>
2018-01-25 11:20:34 +0200

    work on doc

--- src/containers/Documentation/Sections/Inputs.jsx
+++ src/containers/Documentation/Sections/Inputs.jsx
@@ -2,8 +2,78 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { prism } from 'react-syntax-highlighter/styles/prism';
 import { Grid } from 'material-ui';
+import { People } from 'material-ui-icons';
 
 import { CustomInput } from 'components';
+
+const inputsDep = `import { Grid } from 'material-ui';
+import { People } from 'material-ui-icons';
+import { CustomInput } from 'components';`;
+const inputs = `<Grid container>
+    <Grid item xs={12} sm={12} md={4}>
+        <CustomInput
+            labelText="Disabled"
+            id="disabled"
+            formControlProps={{
+                fullWidth: true
+            }}
+            inputProps={{
+                disabled: true
+            }}
+        />
+    </Grid>
+    <Grid item xs={12} sm={12} md={4}>
+        <CustomInput
+            id="regular"
+            inputProps={{
+                placeholder: "Regular"
+            }}
+            formControlProps={{
+                fullWidth: true
+            }}
+        />
+    </Grid>
+    <Grid item xs={12} sm={12} md={4}>
+        <CustomInput
+            labelText="With floating label"
+            id="float"
+            formControlProps={{
+                fullWidth: true
+            }}
+        />
+    </Grid>
+    <Grid item xs={12} sm={12} md={4}>
+        <CustomInput
+            labelText="Success input"
+            id="success"
+            success
+            formControlProps={{
+                fullWidth: true
+            }}
+        />
+    </Grid>
+    <Grid item xs={12} sm={12} md={4}>
+        <CustomInput
+            labelText="Error input"
+            id="error"
+            error
+            formControlProps={{
+                fullWidth: true
+            }}
+        />
+    </Grid>
+    <Grid item xs={12} sm={12} md={4}>
+        <CustomInput
+            icon={People}
+            labelText="With material Icons"
+            id="material"
+            formControlProps={{
+                fullWidth: true
+            }}
+        />
+    </Grid>
+</Grid>`;
+const inputsProp = ``;
 
 class Inputs extends React.Component{
     render(){
@@ -47,6 +117,7 @@ class Inputs extends React.Component{
                         <CustomInput
                             labelText="Success input"
                             id="success"
+                            success
                             formControlProps={{
                                 fullWidth: true
                             }}
@@ -56,6 +127,7 @@ class Inputs extends React.Component{
                         <CustomInput
                             labelText="Error input"
                             id="error"
+                            error
                             formControlProps={{
                                 fullWidth: true
                             }}
@@ -63,15 +135,18 @@ class Inputs extends React.Component{
                     </Grid>
                     <Grid item xs={12} sm={12} md={4}>
                         <CustomInput
-                            labelText="Error input"
-                            id="error"
+                            icon={People}
+                            labelText="With material Icons"
+                            id="material"
                             formControlProps={{
                                 fullWidth: true
                             }}
                         />
                     </Grid>
                 </Grid>
-                <SyntaxHighlighter language="jsx" style={prism}>{`<NameOfTheIcon />`}</SyntaxHighlighter>
+                <SyntaxHighlighter language="jsx" style={prism}>{inputsDep}</SyntaxHighlighter>
+                <SyntaxHighlighter language="jsx" style={prism}>{inputs}</SyntaxHighlighter>
+                <SyntaxHighlighter language="jsx" style={prism}>{inputsProp}</SyntaxHighlighter>
             </div>
         );
     }
