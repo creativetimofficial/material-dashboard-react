bf487d7aa5c59085b139ddb0d19820f9666dd80e
Manu <emanuelnazare@yahoo.ro>
2018-06-12 16:14:18 +0300

    changed from http to https

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ function Footer({ ...props }) {
         <p className={classes.right}>
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
-            <a href="http://www.creative-tim.com" className={classes.a}>
+            <a href="https://www.creative-tim.com" className={classes.a}>
               Creative Tim
             </a>, made with love for a better web
           </span>
