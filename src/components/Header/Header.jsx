e9943d02528f379b4a751b9b48e2ae1f84cef811
Manu <emanuelnazare@yahoo.ro>
2018-09-21 14:32:41 +0300

    Added .jsx in our imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,8 +10,8 @@ import Hidden from "@material-ui/core/Hidden";
 // @material-ui/icons
 import Menu from "@material-ui/icons/Menu";
 // core components
-import HeaderLinks from "./HeaderLinks";
-import Button from "components/CustomButtons/Button";
+import HeaderLinks from "./HeaderLinks.jsx";
+import Button from "components/CustomButtons/Button.jsx";
 
 import headerStyle from "assets/jss/material-dashboard-react/components/headerStyle.jsx";
 
