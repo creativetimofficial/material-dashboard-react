44850fa4108ee7656a98ae7506892e4222fbdc72
Manu <emanuelnazare@yahoo.ro>
2019-02-13 11:57:36 +0200

    - live done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactPixel from "react-facebook-pixel";
 
 // core components
 import Admin from "layouts/Admin.jsx";
@@ -11,16 +11,17 @@ import Documentation from "layouts/Documentation/Documentation.jsx";
 
 import "assets/css/material-dashboard-react.css?v=1.6.0";
 
-const hist = createBrowserHistory();
+const hist = createHashHistory();
 
-ReactGA.initialize("UA-46172202-1");
-ReactGA.set({ page: window.location.pathname + window.location.search });
-ReactGA.pageview(window.location.pathname + window.location.search);
+ReactPixel.init("111649226022273");
+ReactPixel.pageView();
+ReactPixel.fbq("track", "PageView");
 
 hist.listen(location => {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  ReactPixel.pageView();
+  ReactPixel.fbq("track", "PageView");
 });
+
 ReactDOM.render(
   <HashRouter>
     <Switch>
