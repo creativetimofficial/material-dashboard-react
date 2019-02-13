424213f6a584492f088fe593075fd401fbdcee36
Manu <emanuelnazare@yahoo.ro>
2019-02-13 14:23:32 +0200

    Updates

---
+++ precache-manifest.31cdbac69a956d3bfe1adc64a737120e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "d93a89c88c3b20596b79",
+    "url": "/material-dashboard-react/static/css/main.bfe980cb.chunk.css"
+  },
+  {
+    "revision": "869affe5ae044d77438d",
+    "url": "/material-dashboard-react/static/js/runtime~main.869affe5.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "a99bf750b9e26f5cb59e",
+    "url": "/material-dashboard-react/static/js/2.a99bf750.chunk.js"
+  },
+  {
+    "revision": "d93a89c88c3b20596b79",
+    "url": "/material-dashboard-react/static/js/main.d93a89c8.chunk.js"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "a99bf750b9e26f5cb59e",
+    "url": "/material-dashboard-react/static/css/2.b57e5a87.chunk.css"
+  },
+  {
+    "revision": "5fef61eb49ed7e7c115d4282d9d66033",
+    "url": "/material-dashboard-react/index.html"
+  }
+];
