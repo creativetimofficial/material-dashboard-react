e216168be622fd680d4ebe5ed7efcbefddb5c7ab
Manu <emanuelnazare@yahoo.ro>
2019-06-19 10:30:48 +0300

    Updates

---
+++ precache-manifest.963a0d66c6fd3c89092d2dc04cbb2a63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074acae7e5d90a0f9a17e2ba0ecead3f",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "d613bf6a25d57fb050bd",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "984cdc625a35ffaf5e03",
+    "url": "/material-dashboard-react/static/css/main.acf18415.chunk.css"
+  },
+  {
+    "revision": "d613bf6a25d57fb050bd",
+    "url": "/material-dashboard-react/static/js/2.1a5999a3.chunk.js"
+  },
+  {
+    "revision": "984cdc625a35ffaf5e03",
+    "url": "/material-dashboard-react/static/js/main.8e7fc986.chunk.js"
+  },
+  {
+    "revision": "b8556c08b70d6c956861",
+    "url": "/material-dashboard-react/static/js/runtime~main.8f94a1b0.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-dashboard-react/precache-manifest.e0a5497f53812a5f8e2eca3c00aa6bd9.js"
+  "/material-dashboard-react/precache-manifest.963a0d66c6fd3c89092d2dc04cbb2a63.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/material-dashboard-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/material-dashboard-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
