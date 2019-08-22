415971be1ccbab6def119669422d4a8d0db773c2
EINazare <emanuelioannazare@gmail.com>
2019-08-22 13:48:22 +0300

    - Tasks

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,25 +1,8 @@
-/*!
-
-=========================================================
-* Material Dashboard React - v1.7.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import { makeStyles } from "@material-ui/core/styles";
 import Checkbox from "@material-ui/core/Checkbox";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
@@ -32,101 +15,92 @@ import Edit from "@material-ui/icons/Edit";
 import Close from "@material-ui/icons/Close";
 import Check from "@material-ui/icons/Check";
 // core components
-import tasksStyle from "assets/jss/material-dashboard-react/components/tasksStyle.jsx";
+import styles from "assets/jss/material-dashboard-react/components/tasksStyle.js";
 
-class Tasks extends React.Component {
-  state = {
-    checked: this.props.checkedIndexes
-  };
-  handleToggle = value => () => {
-    const { checked } = this.state;
+const useStyles = makeStyles(styles);
+
+export default function Tasks(props) {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([...props.checkedIndexes]);
+  const handleToggle = value => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
-
     if (currentIndex === -1) {
       newChecked.push(value);
     } else {
       newChecked.splice(currentIndex, 1);
     }
-
-    this.setState({
-      checked: newChecked
-    });
+    setChecked(newChecked);
   };
-  render() {
-    const { classes, tasksIndexes, tasks, rtlActive } = this.props;
-    const tableCellClasses = classnames(classes.tableCell, {
-      [classes.tableCellRTL]: rtlActive
-    });
-    return (
-      <Table className={classes.table}>
-        <TableBody>
-          {tasksIndexes.map(value => (
-            <TableRow key={value} className={classes.tableRow}>
-              <TableCell className={tableCellClasses}>
-                <Checkbox
-                  checked={this.state.checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  onClick={this.handleToggle(value)}
-                  checkedIcon={<Check className={classes.checkedIcon} />}
-                  icon={<Check className={classes.uncheckedIcon} />}
-                  classes={{
-                    checked: classes.checked,
-                    root: classes.root
-                  }}
-                />
-              </TableCell>
-              <TableCell className={tableCellClasses}>{tasks[value]}</TableCell>
-              <TableCell className={classes.tableActions}>
-                <Tooltip
-                  id="tooltip-top"
-                  title="Edit Task"
-                  placement="top"
-                  classes={{ tooltip: classes.tooltip }}
+  const { tasksIndexes, tasks, rtlActive } = props;
+  const tableCellClasses = classnames(classes.tableCell, {
+    [classes.tableCellRTL]: rtlActive
+  });
+  return (
+    <Table className={classes.table}>
+      <TableBody>
+        {tasksIndexes.map(value => (
+          <TableRow key={value} className={classes.tableRow}>
+            <TableCell className={tableCellClasses}>
+              <Checkbox
+                checked={checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                onClick={() => handleToggle(value)}
+                checkedIcon={<Check className={classes.checkedIcon} />}
+                icon={<Check className={classes.uncheckedIcon} />}
+                classes={{
+                  checked: classes.checked,
+                  root: classes.root
+                }}
+              />
+            </TableCell>
+            <TableCell className={tableCellClasses}>{tasks[value]}</TableCell>
+            <TableCell className={classes.tableActions}>
+              <Tooltip
+                id="tooltip-top"
+                title="Edit Task"
+                placement="top"
+                classes={{ tooltip: classes.tooltip }}
+              >
+                <IconButton
+                  aria-label="Edit"
+                  className={classes.tableActionButton}
                 >
-                  <IconButton
-                    aria-label="Edit"
-                    className={classes.tableActionButton}
-                  >
-                    <Edit
-                      className={
-                        classes.tableActionButtonIcon + " " + classes.edit
-                      }
-                    />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip
-                  id="tooltip-top-start"
-                  title="Remove"
-                  placement="top"
-                  classes={{ tooltip: classes.tooltip }}
+                  <Edit
+                    className={
+                      classes.tableActionButtonIcon + " " + classes.edit
+                    }
+                  />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                id="tooltip-top-start"
+                title="Remove"
+                placement="top"
+                classes={{ tooltip: classes.tooltip }}
+              >
+                <IconButton
+                  aria-label="Close"
+                  className={classes.tableActionButton}
                 >
-                  <IconButton
-                    aria-label="Close"
-                    className={classes.tableActionButton}
-                  >
-                    <Close
-                      className={
-                        classes.tableActionButtonIcon + " " + classes.close
-                      }
-                    />
-                  </IconButton>
-                </Tooltip>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  }
+                  <Close
+                    className={
+                      classes.tableActionButtonIcon + " " + classes.close
+                    }
+                  />
+                </IconButton>
+              </Tooltip>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
 
 Tasks.propTypes = {
-  classes: PropTypes.object.isRequired,
   tasksIndexes: PropTypes.arrayOf(PropTypes.number),
   tasks: PropTypes.arrayOf(PropTypes.node),
   rtlActive: PropTypes.bool,
   checkedIndexes: PropTypes.array
 };
-
-export default withStyles(tasksStyle)(Tasks);
