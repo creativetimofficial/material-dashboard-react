4bb5a07f8aa079deed5e193c6bb0e0db99e427f8
Asif Shikder <shikdernyc@gmail.com>
2020-03-27 22:53:20 -0400

    updated with airbnb config for starter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,46 @@
 module.exports = {
-  parser: "babel-eslint",
   env: {
+    browser: true,
     es6: true,
-    node: true,
-    browser: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended"
-  ]
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/no-array-index-key': 0,
+    'no-plusplus': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+    'import/prefer-default-export': 0,
+    'react/destructuring-assignment': 0,
+    'react/no-access-state-in-setstate': 0,
+    'max-classes-per-file': 0,
+    'no-underscore-dangle': 0,
+    'react/require-default-props': 0,
+    'react/forbid-prop-types': 0, // TOOD: REFACTOR TO PREVENT OBJECT
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
 };
