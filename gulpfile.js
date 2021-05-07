c5aaca0d4508a55623070d5217f5345e0c16e394
EINazare <emanuelioannazare@gmail.com>
2021-05-07 11:05:13 +0300

    style: run prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
-gulp.task("licenses", async function() {
+gulp.task("licenses", async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
