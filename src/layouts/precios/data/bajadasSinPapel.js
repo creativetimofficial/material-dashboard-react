bd6160acbdd91ba1b3aa2ea8fb767480cc4444a0
Juan Cruz Riera <juancriera94@gmail.com>
2023-05-10 16:25:02 -0300

    First version working

--- src/layouts/precios/data/bajadasSinPapel.js
+++ src/layouts/precios/data/bajadasSinPapel.js
@@ -19,162 +19,121 @@ Coded by www.creative-tim.com
 
 // Material Dashboard 2 React components
 
-import EditableTableCell from "components/EditableTableCell";
-import MDTypography from "components/MDTypography";
-import { TableCell } from "@mui/material";
 
 
-function getTablesAPI(table) {
-  switch (table) {
-    case "bajadasSinPapel":
-      return {
-        title: "Bajadas sin Papel",
-        table: {
-          copias: ["valor1", "valor2", "valor3", "valor4"],
-          "A3+": ["valor1", "valor2", "valor3", "valor4"],
-          banner: ["valor1", "valor2", "valor3", "valor4"],
-          diseños: ["valor1", "valor2", "valor3", "valor4"],
-        },
-      };
-    case "bajadasAlGremio":
-      return {
-        title: "Bajadas al gremio",
-        paperType: "A4",
-        table: {
-          "COPIAS": ["PAPEL", "1", "2 a 25", "26-50"],
-          "350 grs MATE/BRILLANTE": ["4/0 | 4/4", "280 | 340", "230 | 280", "220 | 265"],
-          "300 grs MATE/BRILLANTE": ["4/0 | 4/4", "260 | 320", "195 | 245", "190 | 240"],
-          "200 grs ILUSTRA/OPALINA": ["4/0 | 4/4", "250 | 310", "182 | 232", "177 | 227"],
-          "150 grs BRILLANTE": ["4/0 | 4/4", "190 | 250", "132 | 182", "128 | 178"],
-          "115 grs BRILLANTE": ["4/0 | 4/4", "185 | 225", "127 | 177", "123 | 173"],
-          "OBRA 90 grs": ["4/0 | 4/4", "140 | 200", "105 | 155", "100 | 150"],
-          "Autoad Con/Sin split": ["4/0", "200", "150", "140"],
-          "OPP Blanco/Transp": ["4/0", "350", "285", "280"],
-        },
-      };
-    case "bajadasAlGremioA3":
-      return {
-        title: "Bajadas al gremio A3+",
-        table: {
-          copias: ["valor1", "valor2", "valor3", "valor4"],
-          "A3+": ["valor1", "valor2", "valor3", "valor4"],
-          banner: ["valor1", "valor2", "valor3", "valor4"],
-          diseños: ["valor1", "valor2", "valor3", "valor4"],
-        },
-      };
-    case "troquelado":
-      return {
-        title: "Troquelados",
-        table: {
-          copias: ["valor1", "valor2", "valor3", "valor4"],
-          "A3+": ["valor1", "valor2", "valor3", "valor4"],
-          banner: ["valor1", "valor2", "valor3", "valor4"],
-          diseños: ["valor1", "valor2", "valor3", "valor4"],
-        },
-      };
-    default:
-      return [
+
+function getTablesAPI() {
+
+  return [
+    {
+      paperSize: "Sin Papel",
+      gramajes: [
         {
-          paperSize: "A4",
-          quantity: [
-            {
-              min: 1,
-              max: 1,
-              gramajes: [
-                { gramaje: "150", fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
-                { gramaje: "200", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "250", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "300", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "350", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-              ]
-            },
-            {
-              min: 2, max: 25, gramajes: [
-                { gramaje: "150", fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
-                { gramaje: "200", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "250", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "300", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "350", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-              ]
-            },
-            {
-              min: 26, max: 50, gramajes: [
-                { gramaje: "150", fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
-                { gramaje: "200", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "250", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "300", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "350", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-              ]
-            }
+          gramaje: "A3",
+          quantities: [
+            { min: 101, max: 250, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 251, max: 500, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 501, max: 1000, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+      ],
+    },
+    {
+      paperSize: "A4",
+      gramajes: [
+        {
+          gramaje: "150",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
           ]
         },
         {
-          paperSize: "A3",
-          quantity: [
-            {
-              min: 1, max: 1, gramajes: [
-                { gramaje: "150", fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
-                { gramaje: "200", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "250", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "300", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "350", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-              ]
-            },
-            {
-              min: 2, max: 25, gramajes: [
-                { gramaje: "150", fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
-                { gramaje: "200", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "250", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "300", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "350", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-              ]
-            },
-            {
-              min: 26, max: 50, gramajes: [
-                { gramaje: "150", fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
-                { gramaje: "200", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "250", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "300", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-                { gramaje: "350", fazes: [{ doble: true, price: 30 }, { doble: false, price: 45 }] },
-              ]
-            }
+          gramaje: "200",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
           ]
         },
-      ];
-  }
+        {
+          gramaje: "250",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+        {
+          gramaje: "300",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+        {
+          gramaje: "350",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+      ],
+    },
+    {
+      paperSize: "A3",
+      gramajes: [
+        {
+          gramaje: "150",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+        {
+          gramaje: "200",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+        {
+          gramaje: "250",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+        {
+          gramaje: "300",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+        {
+          gramaje: "350",
+          quantities: [
+            { min: 1, max: 1, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 2, max: 25, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+            { min: 25, max: 50, fazes: [{ doble: true, price: 20 }, { doble: false, price: 15 }] },
+          ]
+        },
+      ],
+    },
+  ];
+
 }
 
-export default function data(editable = false, tableName = "bajadasSinPapel") {
+export default function data() {
   // TODO: Invoke API and fill collumns
-  const response = getTablesAPI(tableName);
-  const title = (editable) ? <EditableTableCell value={response.title} color="white" /> : <MDTypography variant="h6" color="white">{response.title}</MDTypography>;
-  const headers = Object.keys(response.table);
-  const columns = [];
-  headers.forEach((header) => {
-    columns.push({
-      Header: (editable) ? <EditableTableCell value={header} /> : <TableCell>{header}</TableCell>,
-      accessor: header,
-      width: "auto",
-      align: "left",
-    });
-  });
-  const numRows = response.table[headers[0]].length; // assumes all arrays have the same length
-  const rows = [];
-
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < numRows; i++) {
-    const row = {};
-    // eslint-disable-next-line no-restricted-syntax
-    for (const key of headers) {
-      row[key] = (editable) ? <EditableTableCell value={response.table[key][i]} /> : <TableCell>{response.table[key][i]}</TableCell>;
-    }
-    rows.push(row);
-  }
-
-  const table = {
-    title,
-    columns,
-    rows,
-  };
-  return table;
+  const category = getTablesAPI();
+  return category;
 }
--- src/layouts/precios/index.js
+++ src/layouts/precios/index.js
@@ -14,18 +14,13 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import * as React from 'react';
-
+import React, { useState } from "react";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 
 // Material Dashboard 2 React example components
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import PriceTable from "examples/Tables/PriceTable";
 
@@ -33,243 +28,86 @@ import PriceTable from "examples/Tables/PriceTable";
 import bajadasSinPapel from "layouts/precios/data/bajadasSinPapel";
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Wrapper from "layouts/Wrapper";
 
 function Precios() {
-  const { title: titleA, columns: columnsA, rows: rowsA } = bajadasSinPapel(true, "bajadasSinPapel");
-  const { title: titleB, columns: columnsB, rows: rowsB } = bajadasSinPapel(false, "bajadasAlGremio");
-  const { title: titleC, columns: columnsC, rows: rowsC } = bajadasSinPapel(false, "bajadasAlGremioA3");
-  const { title: titleD, columns: columnsD, rows: rowsD } = bajadasSinPapel(false, "troquelado");
+  const categorias = bajadasSinPapel(false, "nuevo");
 
-  const [expanded, setExpanded, expandedSub] = React.useState(false);
+  const [expanded, setExpanded] = useState("");
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
   };
+  var lista = [];
+  categorias.forEach(categoria => {
+    const title = <MDTypography>{categoria.paperSize}</MDTypography>;
+    var gramajes = [];
+    categoria.gramajes.forEach(elemento => {
+      var rows = [];
+      const columns = [
+        { Header: "Cantidad", accessor: "cantidad", align: "center" },
+        { Header: "4/0", accessor: "simpleFaz", align: "center" },
+        { Header: "4/4", accessor: "dobleFaz", align: "center" }
+      ];
+      elemento.quantities.forEach(quantity => {
+        rows.push(
+          {
+            "cantidad": <MDTypography variant="button" color="text" fontWeight="regular">{quantity.min}-{quantity.max}</MDTypography>,
+            "simpleFaz": <MDTypography variant="button" color="text" fontWeight="bold">${quantity.fazes[0].price}</MDTypography>,
+            "dobleFaz": <MDTypography variant="button" color="text" fontWeight="bold">${quantity.fazes[1].price}</MDTypography>
+          });
+      });
+      gramajes.push(<Accordion expanded={expanded === elemento.gramaje + categoria.paperSize} onChange={handleChange(elemento.gramaje + categoria.paperSize)}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            {elemento.gramaje} grs.
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <PriceTable
+            table={{ columns: columns, rows: rows }}
+            isSorted={false}
+            entriesPerPage={false}
+            showTotalEntries={false}
+            noEndBorder
+          />
+        </AccordionDetails>
+      </Accordion>);
 
-  return (
-    <DashboardLayout>
-      <DashboardNavbar />
-
+    });
+    lista.push(
       <MDBox>
-        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
-          >
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-              Tamaño papel
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Accordion expanded={expandedSub === 'panelA'} onChange={handleChange('panelA')}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panelabh-content"
-                id="panelabh-header"
-              >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                  Gramaje
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
+        <MDBox mx={2}
+          mt={3}
+          py={3}
+          px={2}>
+          <MDTypography variant="h6">
+            {title}
+          </MDTypography>
+          {
+            gramajes.map((gramaje) => { return gramaje }
+            )
+          }
+        </MDBox>
+      </MDBox>
+    );
+  });
+  return (
 
-                <Typography>
-                  Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-                  Aliquam eget maximus est, id dignissim quam.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Typography>
-              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-              Aliquam eget maximus est, id dignissim quam.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2bh-content"
-            id="panel2bh-header"
-          >
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
-            <Typography sx={{ color: 'text.secondary' }}>
-              You are currently not an owner
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-              varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-              laoreet.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3bh-content"
-            id="panel3bh-header"
-          >
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-              Advanced settings
-            </Typography>
-            <Typography sx={{ color: 'text.secondary' }}>
-              Filtering has been entirely disabled for whole web server
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-              amet egestas eros, vitae egestas augue. Duis vel est augue.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel4bh-content"
-            id="panel4bh-header"
-          >
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-              amet egestas eros, vitae egestas augue. Duis vel est augue.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </MDBox>
-
-
-      <MDBox py={3}>
-        <MDBox mt={4.5}>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h6" color="white">
-                  {titleA}
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <PriceTable
-                  table={{ columns: columnsA, rows: rowsA }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-              </MDBox>
-            </Card>
-          </Grid>
-        </MDBox>
-      </MDBox>
-      <MDBox py={3}>
-        <MDBox mt={4.5}>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h6" color="white">
-                  {titleB}
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <PriceTable
-                  table={{ columns: columnsB, rows: rowsB }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-              </MDBox>
-            </Card>
-          </Grid>
-        </MDBox>
-      </MDBox>
-      <MDBox py={3}>
-        <MDBox mt={4.5}>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h6" color="white">
-                  {titleC}
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <PriceTable
-                  table={{ columns: columnsC, rows: rowsC }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-              </MDBox>
-            </Card>
-          </Grid>
-        </MDBox>
-      </MDBox>
-      <MDBox py={3}>
-        <MDBox mt={4.5}>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h6" color="white">
-                  {titleD}
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <PriceTable
-                  table={{ columns: columnsD, rows: rowsD }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-              </MDBox>
-            </Card>
-          </Grid>
-        </MDBox>
-      </MDBox>
+    <Wrapper title="Lista de Precios">
+      {
+        lista
+      }
       <Footer />
-    </DashboardLayout>
+    </Wrapper>
+
+
   );
 }
+
 
 export default Precios;
