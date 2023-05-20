e125d2612a4191744b8c2a6ef3689fbca98ae3f0
dostonhack <you@example.com>
2023-05-20 12:42:30 +0500

    remove pattern

--- src/examples/Breadcrumbs/index.js
+++ src/examples/Breadcrumbs/index.js
@@ -1,18 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // react-router-dom components
 import { Link } from "react-router-dom";
 
