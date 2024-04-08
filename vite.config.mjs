5f24be3a16eafc769f25158899f1f8e324ed3c7d
Jedacoding <161793224+Jedacoding@users.noreply.github.com>
2024-04-08 09:39:07 +0800

    fix path

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import jsconfigPaths from "vite-jsconfig-paths";
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+    plugins: [react(), jsconfigPaths()],
   };
 });
