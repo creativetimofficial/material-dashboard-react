bfda1b9a2f657817a605db216e38dd348e415713
Leyian <victor.leyian@student.moringaschool.com>
2024-05-22 04:22:20 +0300

    done

--- src/layouts/tables/data/authorsTableData.js
+++ src/layouts/tables/data/authorsTableData.js
@@ -1,176 +1,218 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/function-component-definition */
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDAvatar from "components/MDAvatar";
-import MDBadge from "components/MDBadge";
-
-// Images
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
 
 export default function data() {
-  const Author = ({ image, name, email }) => (
-    <MDBox display="flex" alignItems="center" lineHeight={1}>
-      <MDAvatar src={image} name={name} size="sm" />
-      <MDBox ml={2} lineHeight={1}>
-        <MDTypography display="block" variant="button" fontWeight="medium">
-          {name}
-        </MDTypography>
-        <MDTypography variant="caption">{email}</MDTypography>
-      </MDBox>
-    </MDBox>
-  );
-
-  const Job = ({ title, description }) => (
-    <MDBox lineHeight={1} textAlign="left">
-      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
-        {title}
-      </MDTypography>
-      <MDTypography variant="caption">{description}</MDTypography>
-    </MDBox>
-  );
+  const imageSrc = "path_to_image"; // replace with the actual image source
 
   return {
     columns: [
-      { Header: "author", accessor: "author", width: "45%", align: "left" },
-      { Header: "function", accessor: "function", align: "left" },
-      { Header: "status", accessor: "status", align: "center" },
-      { Header: "employed", accessor: "employed", align: "center" },
-      { Header: "action", accessor: "action", align: "center" },
+      { Header: "name", accessor: "name", width: "20%", align: "left" },
+      { Header: "price", accessor: "price", width: "15%", align: "right" },
+      { Header: "stock_quantity", accessor: "stock_quantity", width: "15%", align: "center" },
+      { Header: "buying_price", accessor: "buying_price", width: "15%", align: "right" },
+      { Header: "selling_price", accessor: "selling_price", width: "15%", align: "right" },
+      { Header: "store_id", accessor: "store_id", width: "10%", align: "center" },
+      { Header: "image", accessor: "image", width: "10%", align: "center" },
     ],
-
     rows: [
       {
-        author: <Author image={team2} name="John Michael" email="john@creative-tim.com" />,
-        function: <Job title="Manager" description="Organization" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            23/04/18
+        name: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Product Name
           </MDTypography>
         ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
+        price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
           </MDTypography>
         ),
+        stock_quantity: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            100
+          </MDTypography>
+        ),
+        buying_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $50.00
+          </MDTypography>
+        ),
+        selling_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
+          </MDTypography>
+        ),
+        store_id: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Store_123
+          </MDTypography>
+        ),
+        image: <MDBox component="img" src={imageSrc} alt="product image" width="50px" />,
       },
       {
-        author: <Author image={team3} name="Alexa Liras" email="alexa@creative-tim.com" />,
-        function: <Job title="Programator" description="Developer" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="offline" color="dark" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            11/01/19
+        name: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Product Name
           </MDTypography>
         ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
+        price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
           </MDTypography>
         ),
+        stock_quantity: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            100
+          </MDTypography>
+        ),
+        buying_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $50.00
+          </MDTypography>
+        ),
+        selling_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
+          </MDTypography>
+        ),
+        store_id: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Store_123
+          </MDTypography>
+        ),
+        image: <MDBox component="img" src={imageSrc} alt="product image" width="50px" />,
       },
       {
-        author: <Author image={team4} name="Laurent Perrier" email="laurent@creative-tim.com" />,
-        function: <Job title="Executive" description="Projects" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            19/09/17
+        name: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Product Name
           </MDTypography>
         ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
+        price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
           </MDTypography>
         ),
+        stock_quantity: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            100
+          </MDTypography>
+        ),
+        buying_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $50.00
+          </MDTypography>
+        ),
+        selling_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
+          </MDTypography>
+        ),
+        store_id: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Store_123
+          </MDTypography>
+        ),
+        image: <MDBox component="img" src={imageSrc} alt="product image" width="50px" />,
       },
       {
-        author: <Author image={team3} name="Michael Levi" email="michael@creative-tim.com" />,
-        function: <Job title="Programator" description="Developer" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            24/12/08
+        name: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Product Name
           </MDTypography>
         ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
+        price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
           </MDTypography>
         ),
+        stock_quantity: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            100
+          </MDTypography>
+        ),
+        buying_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $50.00
+          </MDTypography>
+        ),
+        selling_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
+          </MDTypography>
+        ),
+        store_id: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Store_123
+          </MDTypography>
+        ),
+        image: <MDBox component="img" src={imageSrc} alt="product image" width="50px" />,
       },
       {
-        author: <Author image={team3} name="Richard Gran" email="richard@creative-tim.com" />,
-        function: <Job title="Manager" description="Executive" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="offline" color="dark" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            04/10/21
+        name: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Product Name
           </MDTypography>
         ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
+        price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
           </MDTypography>
         ),
+        stock_quantity: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            100
+          </MDTypography>
+        ),
+        buying_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $50.00
+          </MDTypography>
+        ),
+        selling_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
+          </MDTypography>
+        ),
+        store_id: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Store_123
+          </MDTypography>
+        ),
+        image: <MDBox component="img" src={imageSrc} alt="product image" width="50px" />,
       },
       {
-        author: <Author image={team4} name="Miriam Eric" email="miriam@creative-tim.com" />,
-        function: <Job title="Programator" description="Developer" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="offline" color="dark" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            14/09/20
+        name: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Product Name
           </MDTypography>
         ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
+        price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
           </MDTypography>
         ),
+        stock_quantity: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            100
+          </MDTypography>
+        ),
+        buying_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $50.00
+          </MDTypography>
+        ),
+        selling_price: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            $99.99
+          </MDTypography>
+        ),
+        store_id: (
+          <MDTypography component="a" href="#" variant="body1" color="text" fontWeight="medium">
+            Store_123
+          </MDTypography>
+        ),
+        image: <MDBox component="img" src={imageSrc} alt="product image" width="50px" />,
       },
     ],
   };
--- src/routes.js
+++ src/routes.js
@@ -36,27 +36,20 @@ Coded by www.creative-tim.com
 */
 
 // Material Dashboard 2 React layouts
-import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
-import Billing from "layouts/billing";
-import RTL from "layouts/rtl";
-import Notifications from "layouts/notifications";
-import Profile from "layouts/profile";
-import SignIn from "layouts/authentication/sign-in";
-import SignUp from "layouts/authentication/sign-up";
 
 // @mui icons
 import Icon from "@mui/material/Icon";
 
 const routes = [
-  {
-    type: "collapse",
-    name: "Dashboard",
-    key: "dashboard",
-    icon: <Icon fontSize="small">dashboard</Icon>,
-    route: "/dashboard",
-    component: <Dashboard />,
-  },
+  // {
+  //   type: "collapse",
+  //   name: "Dashboard",
+  //   key: "dashboard",
+  //   icon: <Icon fontSize="small">dashboard</Icon>,
+  //   route: "/dashboard",
+  //   component: <Dashboard />,
+  // },
   {
     type: "collapse",
     name: "Tables",
@@ -65,54 +58,54 @@ const routes = [
     route: "/tables",
     component: <Tables />,
   },
-  {
-    type: "collapse",
-    name: "Billing",
-    key: "billing",
-    icon: <Icon fontSize="small">receipt_long</Icon>,
-    route: "/billing",
-    component: <Billing />,
-  },
-  {
-    type: "collapse",
-    name: "RTL",
-    key: "rtl",
-    icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
-    route: "/rtl",
-    component: <RTL />,
-  },
-  {
-    type: "collapse",
-    name: "Notifications",
-    key: "notifications",
-    icon: <Icon fontSize="small">notifications</Icon>,
-    route: "/notifications",
-    component: <Notifications />,
-  },
-  {
-    type: "collapse",
-    name: "Profile",
-    key: "profile",
-    icon: <Icon fontSize="small">person</Icon>,
-    route: "/profile",
-    component: <Profile />,
-  },
-  {
-    type: "collapse",
-    name: "Sign In",
-    key: "sign-in",
-    icon: <Icon fontSize="small">login</Icon>,
-    route: "/authentication/sign-in",
-    component: <SignIn />,
-  },
-  {
-    type: "collapse",
-    name: "Sign Up",
-    key: "sign-up",
-    icon: <Icon fontSize="small">assignment</Icon>,
-    route: "/authentication/sign-up",
-    component: <SignUp />,
-  },
+  // {
+  //   type: "collapse",
+  //   name: "Billing",
+  //   key: "billing",
+  //   icon: <Icon fontSize="small">receipt_long</Icon>,
+  //   route: "/billing",
+  //   component: <Billing />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "RTL",
+  //   key: "rtl",
+  //   icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
+  //   route: "/rtl",
+  //   component: <RTL />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Notifications",
+  //   key: "notifications",
+  //   icon: <Icon fontSize="small">notifications</Icon>,
+  //   route: "/notifications",
+  //   component: <Notifications />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Profile",
+  //   key: "profile",
+  //   icon: <Icon fontSize="small">person</Icon>,
+  //   route: "/profile",
+  //   component: <Profile />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Sign In",
+  //   key: "sign-in",
+  //   icon: <Icon fontSize="small">login</Icon>,
+  //   route: "/authentication/sign-in",
+  //   component: <SignIn />,
+  // },
+  // {
+  //   type: "collapse",
+  //   name: "Sign Up",
+  //   key: "sign-up",
+  //   icon: <Icon fontSize="small">assignment</Icon>,
+  //   route: "/authentication/sign-up",
+  //   component: <SignUp />,
+  // },
 ];
 
 export default routes;
