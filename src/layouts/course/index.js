5d95b12600ef90190eaf025ce625db139044215e
Girichand <girichand.jupudi@unswengsoc.com>
2024-07-24 04:33:01 +1000

    finished astra

--- src/layouts/course/index.js
+++ src/layouts/course/index.js
@@ -1,13 +1,14 @@
 // react
-import React from "react";
+import React, { useRef } from "react";
 import { useParams } from "react-router-dom";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { ToggleButton, ToggleButtonGroup, Button } from "@mui/material";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
+import MDButton from "components/MDButton";
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -18,9 +19,28 @@ import StudentsTable from "./students";
 import AssessmentsTable from "./assessments";
 
 function Course() {
+  const { course, term } = useParams();
+
   const [selectedTable, setSelectedTable] = React.useState("students");
   const handleTableChange = (event) => {
     setSelectedTable(event.target.value);
+  };
+
+  const handleAstra = async () => {
+    const res = await fetch(`http://localhost:3000/${course}/${term}/astra`, {
+      method: "GET",
+      headers: {
+        "content-Type": "application/json",
+      },
+    });
+    const blob = await res.blob();
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", `${course}-${term}.csv`);
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode.removeChild(link);
   };
 
   return (
@@ -31,6 +51,11 @@ function Course() {
           <ToggleButton value="students">Students</ToggleButton>
           <ToggleButton value="assessments">Assessments</ToggleButton>
         </ToggleButtonGroup>
+      </MDBox>
+      <MDBox pt={2}>
+        <MDButton variant="contained" onClick={handleAstra}>
+          Export astra csv
+        </MDButton>
       </MDBox>
       <MDBox pt={6} pb={3}>
         <Grid container spacing={6}>
